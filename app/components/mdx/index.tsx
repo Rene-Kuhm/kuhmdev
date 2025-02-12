144ac67572d38baee2753846c5a5b003ea5ba150
René Kuhm <157995252+Rene-Kuhm@users.noreply.github.com>
2025-02-12 19:58:12 -0300

    Completely remove blog-related files and components

--- app/components/mdx/index.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-export { default as Tip } from './Tip';
-export { default as Warning } from './Warning';
-export { default as Info } from './Info';
-export { default as CodeBlock } from '../CodeBlock';
-export { default as Demo } from './Demo';
-export { default as ImageGrid } from './ImageGrid';
