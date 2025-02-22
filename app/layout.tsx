e0efbc6f0d19cdc2defe1f4dfe12801559a21fd9
René Kuhm <157995252+Rene-Kuhm@users.noreply.github.com>
2025-02-22 17:23:24 -0300

    Update Google site verification meta tag in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,7 +81,7 @@ export default function RootLayout({
     <html lang="es" className={inter.className}>
       <head>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <meta name="google-site-verification" content="6HE57J3LrIp5-6EbgJpcpnRjsWYVlGuZnY3mCYd9RWs" />
+        <meta name="google-site-verification" content="bREpAbc1J44xtv1Ex5FAMZMcEesuTU3HHlEd7Q8GXJw" />
       </head>
       <body className="bg-black text-white">
         <Navbar />
